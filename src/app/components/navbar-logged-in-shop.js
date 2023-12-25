dbc8c62afa444866e9e573860e005a429b34f671
MuhammadIqbalAlghifari <iqbalalghifari18283@gmail.com>
2023-12-25 13:26:58 +0700

    added counter

--- src/app/components/navbar-logged-in-shop.js
+++ src/app/components/navbar-logged-in-shop.js
@@ -186,7 +186,8 @@ const NavbarLoggedInShop = () => {
   const activeStyle = linkStyle + ' text-red-600 font-bold'; 
   const nonActiveStyle = linkStyle + ' text-white';
 
-
+  // Calculate the total quantity of all products in the "shop" cart
+  const totalShopQuantity = state.shopCart.reduce((total, item) => total + item.quantity, 0);
 
     return (
 
@@ -198,19 +199,20 @@ const NavbarLoggedInShop = () => {
                             <div className="flex lg:gap-2 gap-1 text-xl sm:text-2xl font-semibold md:text-3xl lg:text-5xl justify-start text-white transition" style={{fontFamily: 'Bebas Neue'}}> KNOCKOUT <h2 style={{color: 'red'}}>KINGS</h2></div>
                             <div className="md:hidden flex items-center gap-1.5">
                                 <button className="text-gray-700 rounded-md outline-none" id="help-sidebar">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-white" onClick={() => toggleSidebar("help")}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7 text-white" onClick={() => toggleSidebar("help")}>
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
                                     </svg>
                                 </button>
 
                                 <button className="text-gray-700 rounded-md outline-none" id="love-cart-list" onClick={() => toggleSidebar("love")}>
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-white">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7 text-white">
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
                                     </svg>
                                 </button>
 
-                                <button className="text-gray-700 rounded-md outline-none" id="shopping-cart-list" onClick={() => toggleSidebar("shop")}>
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-white">
+                                <button className="text-gray-700 rounded-md outline-none relative" id="shopping-cart-list" onClick={() => toggleSidebar("shop")}>
+                                    {totalShopQuantity > 0 && <span className="counter text-white bg-red-600 rounded-full p-1 absolute text-[8px] top-0 right-0">{totalShopQuantity}</span>}
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7 text-white">
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
                                     </svg>
                                 </button>
@@ -312,7 +314,8 @@ const NavbarLoggedInShop = () => {
                             </svg>
                         </button>
 
-                        <button id="shopping-cart-list" onClick={() => toggleSidebar("shop")}>
+                        <button id="shopping-cart-list" className="relative" onClick={() => toggleSidebar("shop")}>
+                            {totalShopQuantity > 0 && <span className="counter text-white bg-red-600 rounded-full p-1 absolute text-[9px] top-0 right-0">{totalShopQuantity}</span>}
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="lg:w-8 lg:h-8 md:w-6 md:h-6 w-4 h-4 text-white">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
                             </svg>
